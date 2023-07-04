2003f499d6101121829c5414a16e109a92412651
Jgrtowy <panjogurcik@outlook.com>
2023-07-04 22:20:43 +0200

    🔗 Github payloads are now stored inside Firestore db, fixes

--- index.ts
+++ index.ts
@@ -2,11 +2,24 @@ import cors from 'cors';
 import dotenv from 'dotenv';
 import express from 'express';
 import rateLimit from 'express-rate-limit';
-
+import admin from 'firebase-admin';
 dotenv.config();
 const app = express();
 app.use(express.json());
 app.use(cors());
+
+const serviceAccount = {
+    privateKey: process.env.FIRESTORE_PRIVATE_KEY,
+    clientEmail: process.env.FIRESTORE_EMAIL,
+    projectId: 'githubwebhooksstorage',
+} satisfies admin.ServiceAccount;
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: 'https://githubwebhooksstorage.firebaseio.com',
+});
+
+export const firestore = admin.firestore();
 
 const limiter = rateLimit({
     windowMs: 10 * 60000,
@@ -17,6 +30,7 @@ const limiter = rateLimit({
 });
 app.use(limiter);
 
+import { assert } from 'console';
 import github from './routes/github.js';
 import netlify from './routes/netlify.js';
 import rcon from './routes/rcon.js';
@@ -29,3 +43,4 @@ app.use('/netlify', netlify);
 app.use('/github', github);
 
 app.listen(process.env.PORT || 5000);
+console.log(`Listening on port ${process.env.PORT || 5000}`);
--- lib/sendPayload.ts
+++ lib/sendPayload.ts
@@ -1,28 +1,23 @@
 import { MessageBuilder, Webhook } from 'discord-webhook-node';
 import { config } from 'dotenv';
+import { firestore } from '../index.js';
+import { sendErrorWebhook } from './errorWebhook.js';
 config();
 
-const url: string = process.env.PAYLOAD_WEBHOOK;
-const hook = new Webhook(url);
-
 export const sendPayload = async (payload: any, headers: any) => {
-    let msg = JSON.stringify(payload);
-    if (msg.length > 1900) {
-        msg = msg.slice(0, 1900);
-        msg += `
-        ...`;
+    const url: string = process.env.PAYLOAD_WEBHOOK;
+    const hook = new Webhook(url);
+    try {
+        // Create a new document in Firestore
+        const data = { payload, headers };
+        const docRef = firestore.collection(payload.sender.login).doc();
+        await docRef.set(data);
+
+        // Send a webhook to Discord
+        const payloadEmbed = new MessageBuilder().setAuthor(payload.sender.login, payload.sender.avatar_url, payload.sender.html_url).setDescription(`Payload from ${payload.sender.login} with id: ${docRef.id}`).setColor('#00ff00').setTimestamp();
+        await hook.send(payloadEmbed);
+    } catch (error) {
+        console.error('Error:', error);
+        sendErrorWebhook(error);
     }
-    const payloadEmbed = new MessageBuilder()
-        .setAuthor(payload.sender.login, payload.sender.avatar_url, payload.sender.html_url)
-        .setDescription(`\`\`\`${JSON.stringify(msg)}\`\`\``)
-        .setColor('#00ff00')
-        .setTimestamp();
-    await hook.send(payloadEmbed);
-    const headerEmbed = new MessageBuilder()
-        .setAuthor(payload.sender.login, payload.sender.avatar_url, payload.sender.html_url)
-        .setDescription(`\`\`\`${JSON.stringify(headers)}\`\`\``)
-        .setColor('#0000ff')
-        .setTimestamp();
-    await hook.send(headerEmbed);
-    return;
 };
